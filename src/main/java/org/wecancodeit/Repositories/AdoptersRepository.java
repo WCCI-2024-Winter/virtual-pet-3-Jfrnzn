880bad02d308e39dfe197beb2017d0eafd31ef8a
jfrnzn <jfranzenjr@gmail.com>
2024-03-18 15:29:54 -0400

    EOD Commit, repositories created.

---
+++ src/main/java/org/wecancodeit/Repositories/AdoptersRepository.java
@@ -0,0 +1,7 @@
+package org.wecancodeit.Repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.wecancodeit.Models.AdoptersModel;
+
+public interface AdoptersRepository extends CrudRepository<AdoptersModel, Long>{
+}
